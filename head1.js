718934217b7ca813c6144c147cc64ca56e01c144
Amanda Shalansky <ashalansky20@hotmail.com>
2019-07-29 20:56:53 +0000

    updated refactor and export modules

--- head1.js
+++ head1.js
@@ -1,18 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-   console.log(`Assertion Passed: ${actual} === ${expected}`);
-} else
-   console.log(`Assertion Failed: ${actual} !== ${expected}`);
-
+const assertEqual = require('./assertEqual')
+const head = function (array) {
+    return array[0];
 };
 
-const head = function(array) {
-  if (array.length <= 0) {
-  return;
-} else {
-  return head[0];
-}
-};
-
-assertEqual(head([5,6,7]), 5);
+assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = head;

02dfee5f1b18d14d1e00348e223adadaba4504ba
Amanda Shalansky <ashalansky20@hotmail.com>
2019-07-29 20:56:25 +0000

    updated refactor and export modules

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,11 @@
-const assertEqual = function(actual, expected) {
-   if (actual === expected) {
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
     console.log(`Assertion Passed: ${actual} === ${expected}`);
- } else
+  } else
     console.log(`Assertion Failed: ${actual} !== ${expected}`);
 
-};
+}
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+
+
+module.exports = assertEqual;

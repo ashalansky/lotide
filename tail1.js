53cb64cb88987ccf1fffaa352d8c5522e81922ce
Amanda Shalansky <ashalansky20@hotmail.com>
2019-07-29 21:10:49 +0000

    tail refactor

--- tail1.js
+++ tail1.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-   console.log(`Assertion Passed: ${actual} === ${expected}`);
-} else
-   console.log(`Assertion Failed: ${actual} !== ${expected}`);
-
-};
+const assertEqual = require('./assertEqual');
 const array = [1,2,3];
 const tail = function(array) {
   if (array.length > 1) {
@@ -16,3 +10,4 @@ const tail = function(array) {
 tail(array);
 // assertEqual(tail([1]));
 
+module.exports = tail;
